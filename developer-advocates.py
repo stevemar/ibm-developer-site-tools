ac8a33b141b1b19b5ca12f87160b0793ac2b3da7
Steve Martinelli <s.martinelli@gmail.com>
2018-12-27 16:32:54 -0500

    add script to find advocate info and lookup in ldap

--- developer-advocates.py
+++ developer-advocates.py
@@ -1,13 +1,47 @@
-import urllib2
+# So we post advocates online, but sometimes they leave the company
+# this should look up the advocates and see if they're still
+# in the company LDAP.
+
+# Maybe one day integrate this into a daily cron job that goes into
+# wrike and actually removes the profile and webpage, but for now
+# this was an exercise in using BeautifulSoup and pyldap just for
+# my own benefit.
+
+import urllib.request as urllib2
 from bs4 import BeautifulSoup
 import html2text
+import ldap
 
+ldap_uri = 'ldap://bluepages.ibm.com'
 page = urllib2.urlopen('https://developer.ibm.com/profiles/')
 soup = BeautifulSoup(page, 'html.parser')
 
+print("Listing advocates...")
+
 # find top buttons
 advocates = []
-advocate_divs = soup.findAll('div', attrs={'class' : 'profile-picture__wrap'})
-for advocate_div in advocate_divs:
-    print advocate_div.text
-    advocates.add(advocate_div.text)
+advocate_h3s = soup.findAll('h3')
+for advocate_h3 in advocate_h3s:
+    advocates.append(advocate_h3.text)
+    print(advocate_h3.text)
+
+print("Looking up advocates...")
+
+con = ldap.initialize(ldap_uri)
+ldap_base = 'ou=bluepages,o=ibm.com'
+
+no_entries = []
+for advocate_name in advocates:
+    # NOTE: Search by email or full name (cn)
+    #query = "(email=" + advocate_email + ")"
+    query = "(cn=" + advocate_name + ")"
+
+    try:
+        result = con.search_s(ldap_base, ldap.SCOPE_SUBTREE, query)
+        # The user was deleted from the LDAP if no result
+        if not result:
+            no_entries.append(advocate_name)
+    except:
+        print("ERROR: could not find " + advocate_name)
+
+print(no_entries)
